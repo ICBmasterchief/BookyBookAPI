4512f39c0c4b879b3babfd73c4c8be6b4f25fd36
ICBmasterchief <ignacioelincreible@hotmail.com>
2024-05-06 01:28:03 +0200

    BorrowingUpdateDTO modified

--- BookyBook.Models/BorrowingUpdateDTO.cs
+++ BookyBook.Models/BorrowingUpdateDTO.cs
@@ -7,7 +7,7 @@ public class BorrowingUpdateDTO
 { 
    
    [DataType(DataType.DateTime, ErrorMessage = "La fecha de devolución tiene que ponerse en este formato: año,mes,dia (2024,04,08)")]
-    public DateTime ReturnedDate { get; set; }
+    public DateTime? ReturnedDate { get; set; }
 
     public bool Returned { get; set; }
 
